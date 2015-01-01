470e6cc4fb2849bda8ce4fcb7b761a5834d8e538
Brandon Smith <brandon@praesid.io>
2015-01-01 15:28:00 -0800

    Enforce the validity of DecayConfig args

--- src/main/java/io/praesid/livestats/DecayConfig.java
+++ src/main/java/io/praesid/livestats/DecayConfig.java
@@ -1,5 +1,6 @@
 package io.praesid.livestats;
 
+import com.google.common.base.Preconditions;
 import lombok.ToString;
 
 import java.time.Duration;
@@ -7,7 +8,7 @@ import java.time.Duration;
 @ToString
 public class DecayConfig {
 
-    public static final DecayConfig NEVER = new DecayConfig(1, Duration.ofNanos(0)) {
+    public static final DecayConfig NEVER = new DecayConfig() {
         @Override
         public String toString() {
             return "NEVER";
@@ -18,7 +19,16 @@ public class DecayConfig {
     public final long period;
 
     public DecayConfig(final double multiplier, final Duration period) {
+        Preconditions.checkArgument(multiplier >= 0, "Multiplier must be >= 0");
+        Preconditions.checkArgument(multiplier < 1, "Multiplier must be < 1");
+        Preconditions.checkNotNull(period, "Period must be specified");
+        Preconditions.checkArgument(!period.isNegative() && !period.isZero(), "Period must be positive");
         this.multiplier = multiplier;
         this.period = period.toNanos();
+    }
+
+    private DecayConfig() {
+        multiplier = 1;
+        period = 0;
     }
 }
