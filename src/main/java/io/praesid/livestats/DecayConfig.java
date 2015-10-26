aaba86df649c3d865f00f39cad54c8d88f4d8f7e
Sean Cassidy <sean@praesid.io>
2015-10-26 13:56:55 -0700

    Add some documentation about DecayConfig

--- src/main/java/io/praesid/livestats/DecayConfig.java
+++ src/main/java/io/praesid/livestats/DecayConfig.java
@@ -15,6 +15,12 @@ public final class DecayConfig {
     public final double multiplier;
     public final long period;
 
+    /**
+     * DecayConfig constructor with no period. This is best used when you want to decay values exponentially over time
+     * and the values will never be "invalid". A typical value for "multiplier" is 0.95.
+     *
+     * @param multiplier The exponential moving average coefficient
+     */
     public DecayConfig(final double multiplier) {
         Preconditions.checkArgument(multiplier >= 0, "Multiplier must be >= 0");
         Preconditions.checkArgument(multiplier < 1, "Multiplier must be < 1");
@@ -22,6 +28,14 @@ public final class DecayConfig {
         this.period = 0;
     }
 
+    /**
+     * DecayConfig constructor with a period. This is best used when the data has a strict time bound, where it would
+     * not make sense to keep values around forever. A typical value for "multiplier" is 0.95 and the period depends
+     * on how long the data is valid.
+     *
+     * @param multiplier The exponential moving average coefficient
+     * @param period The time period where the data is valid
+     */
     public DecayConfig(final double multiplier, final Duration period) {
         Preconditions.checkArgument(multiplier >= 0, "Multiplier must be >= 0");
         Preconditions.checkArgument(multiplier < 1, "Multiplier must be < 1");
